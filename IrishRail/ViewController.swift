a5df7b66d4ffd49de67e9864b8596568aceb1691
Boris Hristov <b.hristov@credoweb.bg>
2020-07-30 15:00:27 +0300

    Initial Commit

---
+++ IrishRail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IrishRail
+//
+//  Created by Boris Hristov on 30.07.20.
+//  Copyright © 2020 bh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IrishRailTests/IrishRailTests.swift
@@ -0,0 +1,34 @@
+//
+//  IrishRailTests.swift
+//  IrishRailTests
+//
+//  Created by Boris Hristov on 30.07.20.
+//  Copyright © 2020 bh. All rights reserved.
+//
+
+import XCTest
+@testable import IrishRail
+
+class IrishRailTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
